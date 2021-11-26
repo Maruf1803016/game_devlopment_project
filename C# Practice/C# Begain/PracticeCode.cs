5c63e9acaa170d84ef8a5cdd630551bdaebd26c6
Maruf1803016 <95102146+Maruf1803016@users.noreply.github.com>
2021-11-27 01:39:57 +0600

    New Added
    C# First Code Pushed

---
+++ C# Practice/C# Begain/PracticeCode.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class one{
+   public static void Main ()
+    {
+        Console.WriteLine("Ohaio");
+        int a=5,b=4;
+
+        if(a%2==0)
+        {
+            Console.WriteLine(" A is a Even number and B is Odd");
+        }
+        else if(b%2==0)
+        {
+            Console.WriteLine(" B is a Even number and A is Odd");
+        }
+       
+
+    }
+}
